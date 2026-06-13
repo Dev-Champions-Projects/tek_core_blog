08548cc3ed6c400b9ced47d473124e2263c77d7d
Promise Duke <promiseduke@gmail.com>
2026-06-13 09:35:20 +0100

    force-dynamic on signin

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,9 @@
 import SignUpForm from "@/components/signup-form";
 import { requireNoAuth } from "@/lib/auth-utils";
 
+// This page must be rendered dynamically because it checks auth headers
+export const dynamic = "force-dynamic";
+
 export default async function SignInPage() {
   await requireNoAuth();
 
